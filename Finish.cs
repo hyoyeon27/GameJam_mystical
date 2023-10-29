86eda3390880862df042b31bb2e8a73af3029c3f
Avery Ryu <118397782+averyryu@users.noreply.github.com>
2023-10-29 11:36:05 +0000

    Add Finish.cs

---
+++ Finish.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource finishiSound;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        finishiSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collison) 
+    {
+        if(collison.gameObject.name == "Player")
+        {
+            finishiSound.Play();
+            CompleteLevel();
+        }
+    }
+    
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
