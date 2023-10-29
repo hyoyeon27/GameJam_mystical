62f0f6e8e15006b4e0f7afe00b9b3cbd0fbb299e
ykmhere <zt22740@bristol.ac.uk>
2023-10-29 20:06:59 +0000

    melon update

--- Assets/Script/ItemCollector.cs
+++ Assets/Script/ItemCollector.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+    private int melons = 0;
 
     [SerializeField] private Text cherriesText;
+    [SerializeField] private Text melonsText;
     [SerializeField] private AudioSource collectSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,6 +20,13 @@ public class ItemCollector : MonoBehaviour
             Destroy(collision.gameObject);
             cherries++;
             cherriesText.text = "Cherries: " + cherries;
+        }
+        else if (collision.gameObject.CompareTag("Melon"))
+        {
+            collectSoundEffect.Play();
+            Destroy(collision.gameObject);
+            melons++;
+            melonsText.text = "Melons: " + melons;
         }
     }
 
